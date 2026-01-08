6482fa041b54aa278adf759c78e00afaf5c82277
Sri Venkateswaran <srivenkateswaran6002@gmail.com>
2026-01-08 09:49:20 +0530

    Update api for env var

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const BASE_URL = "http://localhost:8000/api"
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_DB_URL
 
 export async function fetchAllEvents() {
     try {
