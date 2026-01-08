b4a4fb330c1871882c1907eb5be5085bea77a1b3
Sri Venkateswaran <srivenkateswaran6002@gmail.com>
2026-01-08 21:36:45 +0530

    Added API Methods For Authentication

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -2,9 +2,13 @@ import axios from "axios"
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_DB_URL
 
+function getToken() {
+    return localStorage.getItem('authToken')
+}
+
 export async function fetchAllEvents() {
     try {
-        const res = await axios.get(`${BASE_URL}/events/`)
+        const res = await axios.get(`${BASE_URL}/events/` , {headers : {Authorization : `Token ${getToken()}`}})
         return res.data
     } catch (err) {
         console.error(`Failed to fetch events from backend:`, err)
@@ -14,7 +18,7 @@ export async function fetchAllEvents() {
 
 export async function fetchEventById(id) {
     try {
-        const res = await axios.get(`${BASE_URL}/events/${id}/`)
+        const res = await axios.get(`${BASE_URL}/events/${id}/` , {headers : {Authorization : `Token ${getToken()}`}})
         return res.data
     } catch (err) {
         console.error(`Failed to fetch event with id ${id} from backend:`, err)
@@ -24,7 +28,7 @@ export async function fetchEventById(id) {
 
 export async function createEvent(eventData) {
     try {
-        const res = await axios.post(`${BASE_URL}/events/`, eventData)
+        const res = await axios.post(`${BASE_URL}/events/`, eventData , {headers : {Authorization : `Token ${getToken()}`}})
         return res.data
     } catch (err) {
         console.error(`Failed to create event:`, err)
@@ -34,7 +38,7 @@ export async function createEvent(eventData) {
 
 export async function updateEvent(id, eventData) {
     try {
-        const res = await axios.put(`${BASE_URL}/events/${id}/`, eventData)
+        const res = await axios.put(`${BASE_URL}/events/${id}/`, eventData , {headers : {Authorization : `Token ${getToken()}`}})
         return res.data
     } catch (err) {
         console.error(`Failed to update event with id ${id}:`, err)
@@ -44,7 +48,7 @@ export async function updateEvent(id, eventData) {
 
 export async function deleteEvent(id) {
     try {
-        const res = await axios.delete(`${BASE_URL}/events/${id}/`)
+        const res = await axios.delete(`${BASE_URL}/events/${id}/` , {headers : {Authorization : `Token ${getToken()}`}})
         return res.data
     } catch (err) {
         console.error(`Failed to delete event with id ${id}:`, err)
@@ -54,13 +58,38 @@ export async function deleteEvent(id) {
 
 export async function searchEvents(title) {
     try {
-        const res = await axios.get(`${BASE_URL}/events/`, {
-            params: { title }
-        })
+        const res = await axios.get(`${BASE_URL}/events/`, {params: { title } , headers : {Authorization : `Token ${getToken()}`}} )
         return res.data
     }
     catch (err) {
         console.error(`Failed to search events with title ${title}:`, err)
+        throw err
+    }
+}
+
+export function logout() {
+    localStorage.removeItem('authToken')
+}
+
+export async function login(username , password){
+    try {
+        const res = await axios.post(`${BASE_URL}/auth/login/` , {username , password})
+        localStorage.setItem('authToken' , res.data.token)
+        return [res.data.username , res.data.id]
+    }
+    catch (err) {
+        console.error("Failed to login." , err)
+        throw err
+    }
+}
+
+export async function register(username , password) {
+    try {
+        const res = await axios.post(`${BASE_URL}/auth/register/` , {username , password})
+        return username
+    }
+    catch (err) {
+        console.error("Failed to register" , err)
         throw err
     }
 }
